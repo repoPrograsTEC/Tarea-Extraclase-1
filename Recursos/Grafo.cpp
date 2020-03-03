f6bf4dc23bd63848252767dcd132184b013a5e29
repoPrograsTEC <repoprograstec@gmail.com>
2020-03-02 18:05:12 -0600

    Arreglando clases

--- Recursos/Grafo.cpp
+++ Recursos/Grafo.cpp
@@ -62,31 +62,35 @@ bool State::operator<(const State &b) const {
 
 //************************************************//
 
+class Dijkstra{
+public:
+    static int algoritmo(int, int, const Graph&);
+};
 
-int algoritmo(const int begin, const int end, const Graph graph){
-    priority_queue<State> pq; // La cola de prioridad.
-    vector<int> Dist(graph.V, oo); // La distancia hacia todos los vertices. Inicialmente para cada vertice su valor es infinito.
-    vector<bool> mark(graph.V, false); // Este arreglo nos permitira determinar los nodos procesados.
+int Dijkstra :: algoritmo(const int begin, const int end, const Graph& graph){
+    priority_queue<State> pq;                                               // La cola de prioridad.
+    vector<int> Dist(graph.V, oo);                                          // La distancia hacia todos los vertices. Inicialmente para cada vertice su valor es infinito.
+    vector<bool> mark(graph.V, false);                                // Este arreglo nos permitira determinar los nodos procesados.
 
-    Dist[begin] = 0; // Valor inicial del vertice de partida.
-    pq.push(State(begin, 0)); // Agregamos el primer elemento, que no es mas que el vertice de partida.
-    while(!pq.empty()) { // Mientras existan vertices por procesar.
-        State st = pq.top(); pq.pop(); // Se desencola el elemento minimo.
-        mark[st.node] = true; // Se marca el nodo como visitado.
-        if (st.node == end)
-            return st.cost; // Retornamos el valor del camino, hemos llegado al vertice destino.
-
+    Dist[begin] = 0;                                                        // Valor inicial del vertice de partida.
+    pq.push(State(begin, 0));                                      // Agregamos el primer elemento, que no es mas que el vertice de partida.
+    while(!pq.empty()) {                                                   // Mientras existan vertices por procesar.
+        State st = pq.top(); pq.pop();                                     // Se desencola el elemento minimo.
+        mark[st.node] = true;                                              // Se marca el nodo como visitado.
+        if (st.node == end) {
+            return st.cost;                                                // Retornamos el valor del camino, hemos llegado al vertice destino.
+        }
         int T = (int)graph.G[st.node].size();
-        for(int i = 0; i < T; ++i) { // Se recorren las adyacencias de "a".
-            // Si no ha sido procesado el vertice "vi" y la distancia hacia "vi" es menor a la distancia
-            // en Dist entonces hemos encontrado un camino mas corto a "vi".
+        for(int i = 0; i < T; ++i) {                                       // Se recorren las adyacencias de "a".
+                                                                           // Si no ha sido procesado el vertice "vi" y la distancia hacia "vi" es menor a la distancia
+                                                                           // en Dist entonces hemos encontrado un camino mas corto a "vi".
             if (!mark[graph.G[st.node][i].node] && ((Dist[st.node] + graph.G[st.node][i].cost) < Dist[graph.G[st.node][i].node])){
                 Dist[graph.G[st.node][i].node] = st.cost + graph.G[st.node][i].cost;
                 pq.push(State(graph.G[st.node][i].node, st.cost + graph.G[st.node][i].cost));
             }
         }
     }
-    return -1; // Si no se puede llegar al destino, retornar -1.
+    return -1;
 }
 
 
@@ -98,7 +102,7 @@ public:
     int comienzo, fin;
     void definirGrafo(Graph& graph);
     void cargarGrafo(Graph & graph);
-    void Dijkstra(Graph graph);
+    void Dijkstra(const Graph& graph);
 };
 
 void Programa:: definirGrafo(Graph& graph){
@@ -127,13 +131,18 @@ void Programa :: cargarGrafo(Graph & graph){
     }
 }
 
-void Programa :: Dijkstra(Graph graph){
+void Programa :: Dijkstra(const Graph& graph){
     cout << "Ingrese Vertice Inicial: " << endl;
     cin >> comienzo;
     cout << "Ingrese Vertice Final: " << endl;
     cin >> fin;
-    int n = algoritmo(comienzo, fin, graph);
-    cout << "Longitud del Camino mas Corto: " << n << endl;
+    int n = Dijkstra::algoritmo(comienzo, fin, graph);  //Accediendo al algoritmo de Dijkstra
+    if (n >= 0){
+        cout << "El valor de la ruta es: " + to_string(n) << endl;
+    } else {
+        cout << "No se pudo encontrar la ruta porque el vertice ingresado no existe o no hay ruta entre vertices" << endl;
+        cout << "Intente de nuevo!" << endl << endl;
+    }
 }
 
 //************************************************//
